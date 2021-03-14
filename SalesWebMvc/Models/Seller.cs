2ea2cae06e7e0cf1304eb10d90ee83a3778dc33b
Joel-Nogueira <joelnogueira733@gmail.com>
2021-03-14 12:58:05 -0300

    Seeding Service

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -12,19 +12,23 @@ namespace SalesWebMvc.Models
         public DateTime BirthDate { get; set; }
         public Double BaseSalary { get; set; }
 
+        public Department Department{ get; set; }
+
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
         public Seller()
         {
         }
 
-        public Seller(int id, string name, string email, DateTime birthDate, double baseSalary)
+        public Seller(int id, string name, string email, DateTime birthDate, 
+            double baseSalary, Department department)
         {
             Id = id;
             Name = name;
             Email = email;
             BirthDate = birthDate;
             BaseSalary = baseSalary;
+            Department = department;
         }
 
         public void AddSales(SalesRecord sr)
